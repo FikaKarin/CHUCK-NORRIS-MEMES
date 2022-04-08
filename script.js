579d40689242a045799526894bd0dbf23444f4da
FikaKarin <mailtolundqvist@gmail.com>
2022-04-08 12:16:32 +0200

    commentary and cleaning up code

--- script.js
+++ script.js
@@ -13,79 +13,44 @@ async function getDataFromStrapi() {
   //tom variabel för att lägga värdet av hämtade memes
   let output = "";
 
-  //variabel för edit-table
-  let editOutput = "<table>";
-
-//loopar igenom objekten i meme-databasen
-  if (Array.isArray(myObjekt.data)) {
-    //Anropa generateRow för att skapa en HEader-rad
-    editOutput += generateRow(myObjekt.data[0].attributes, null, true);
-
     //Skapar en forEach loop för varje element i dataArrayen
     myObjekt.data.forEach((element) => {
       let attr = element.attributes;
-      let obj = myObjekt.data.attributes;
-      let objId = myObjekt.data.id;
-
-      //for-loop för objekt: skriv ut idnr, name, level, deleteknapp, editknapp.
+      //kontroll av utskrift
       for (x in attr) {
         console.log(x + ": " + attr[x]);
 
-            //Skriver Output string
-            editOutput += generateRow(obj, element.id, false);
-
-
       }
+      //skriver ut varje element i databas tillsammans med delete och edit-funktion
       output += `<div>
-      <div>Nr: ${element.id} <br> <div id="editName">Namn: ${attr.meme}</div> 
+      <div><div id="editName">Meme: ${attr.meme}</div> 
       <br> 
       <div id="editLevel">Level: ${attr.chucklevel}</div>
       <br>
-      <button onclick = "deleteConfirm('http://localhost:1337/api/memes/${element.id}', ${element.id});">DELETE</button>
-      <button onclick="editMeme('http://localhost:1337/api/memes/${element.id}', ${element.id});">EDIT</button>
+      <button onclick = "deleteConfirm('http://localhost:1337/api/memes/${element.id}', ${element.id});">Delete</button>
+      <button onclick="editMeme('http://localhost:1337/api/memes/${element.id}', ${element.id});">Edit</button>
       <div id="editMeme${element.id}" ></div> 
       </div>
       <br><br>`;
     });
-  
-    //annars skriv skriv ut objektet attribut
-  } else {
-    let obj = myObjekt.data.attributes;
-    for (x in obj) {
-      console.log(x + ": " + obj[x]);
-    }
-    output += `<div>Namn: ${obj.meme} <br> Level: ${obj.chucklevel}</div>`;
-    
-    //Skapa en Header Rad
-    editOutput += generateRow(obj, null, true);
-
-    //Skriver Output string
-    editOutput += generateRow(obj, myObjekt.data.id, false);
-  
-  }
-     //Avsluta <table> tag
-     output += "</table>";
 
   document.getElementById("memeFetched").innerHTML = output;
 }
 
-
+//funktion för att hämta token för validering
 async function getToken() {
-  //1. göra inloggningsförsök för att få en Token returnerad
-  //2. samla data och skapa ett objekt av dessa
-  //3. skicka iväg JSON till API
 
   //variabel med url till användare
   const urlUser = "http://localhost:1337/api/auth/local/";
 
-  //variabler innehållande ifylld användare och lösenord
+  //hämtar data från fält
   const user = document.getElementById("user").value;
   const pass = document.getElementById("pass").value;
 
   //objekt innehållande attribut med värdet av variabel user och pass
   let userObject = {
     identifier: user,
-    password: pass,
+    password: pass
   };
 
   //anropar API med inloggningsdata
@@ -93,26 +58,27 @@ async function getToken() {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-
     },
     body: JSON.stringify(userObject),
   });
 
+  //variabel skapas/inväntar userResponse som json-objekt
   let userJson = await userResponse.json();
 
   console.log(userJson);
 
+  //om userResponse får token, returnera token
   if (userJson.jwt) return userJson.jwt;
-
 }
 
 
 //funktion för att lägga till meme i databasen
 async function postData() {
-  
+  //variabel innehåller returvärdet getToken()
   let token = await getToken();
-  if(!token) return;
-  
+  //returnera ur funktionen om getToken() ej körs
+  if (!token) return;
+
   //variabel innehållande databasen
   const urlMeme = "http://localhost:1337/api/memes/";
 
@@ -120,7 +86,7 @@ async function postData() {
   const name = document.getElementById("name").value;
   const level = document.getElementById("level").value;
 
-  //objekt med data innehållande attribut för name och level
+  //objekt med data innehållande attributvärde för meme och chucklevel
   let memeObjekt = {
     data: {
       meme: name,
@@ -128,60 +94,60 @@ async function postData() {
     },
   };
 
-  //anropar API med memes med metod POST för att kunna lägga till
+  //anropar API med memes med metod POST för att kunna lägga till i databasen
   let memeResponse = await fetch(urlMeme, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer " + token
+      Authorization: "Bearer " + token,
     },
     body: JSON.stringify(memeObjekt),
   });
+  //variabel skapas/inväntar memeResponse som json-objekt
   let memeJson = await memeResponse.json();
   console.log(memeJson);
 
-  //skriver ut värdet av name och level i id: addedMemes
-  document.getElementById('addedMemes').innerHTML = name + "<br>" + level;
-  
+  //skriver ut värdet av name och level i div med id "addedMemes"
+  document.getElementById("addedMemes").innerHTML = name + "<br>" + level;
+
   //skriver ut databas igen efter tillägg
   getDataFromStrapi();
 }
 
-
 //Random funktion som skriver ut random memes.
 async function randomMemes() {
- 
-
   let url = "http://localhost:1337/api/memes";
 
+  //variabel innehållande hämtad url
   let stringResponse = await fetch(url);
+  //gör stringResponse till json-objekt
   let myObjekt = await stringResponse.json();
-
-  let memeResult = Math.floor(Math.random() * (myObjekt.data.length - 0) ) +0;
+  //variabel innehållande random index-värde från databas
+  let memeResult = Math.floor(Math.random() * (myObjekt.data.length - 0)) + 0;
   console.log(memeResult);
-
-  document.getElementById('randomMeme').innerHTML = myObjekt.data[memeResult].attributes.meme + "<br>" + "Level: " + myObjekt.data[memeResult].attributes.chucklevel;
-  
+  //skriver ut random meme och dess level
+  document.getElementById("randomMeme").innerHTML =
+    myObjekt.data[memeResult].attributes.meme +
+    "<br>" +
+    "Level: " +
+    myObjekt.data[memeResult].attributes.chucklevel;
 }
 
-
+//funktion för editering
 async function editMeme(url, x) {
-  let test = true;
   let output = "";
 
-  // Hämtar data från fält
+  // Hämtar data från edit-fält
   const name = document.getElementById("editName").value;
-  // const level = parseInt(document.getElementById("editLevel").value);
 
-  if (test) {
     output += `<div>
     <h1>EDIT MEME </h1>
     <div>
-            <h1>Ange användarnamn och lösenord för att kunna lägga till memes: </h1>
-            <label for="user">Användarnamn:</label>
+            <h1>Enter username and password to edit meme: </h1>
+            <label for="user">Username:</label>
             <input type="text" name="user" id="user">
     
-            <label for="pass">Lösenord:</label>
+            <label for="pass">Password:</label>
             <input type="text" name="pass" id="pass">
         </div><br><br>
     <label for="name" >MEME: </label><br>
@@ -190,15 +156,13 @@ async function editMeme(url, x) {
     
 
     <button onclick="updatePost('${url}');">UPDATE</button>
-  </div>`
-  }
-
-  document.getElementById('editMeme'+x).innerHTML = output;
+  </div>`;
   
+
+  document.getElementById("editMeme" + x).innerHTML = output;
 }
 
 async function updatePost(url) {
-
   //Hämta Token från GetToken()
   //Om ingen Token returneras, avbryt funktionen
   let token = await getToken();
@@ -206,120 +170,69 @@ async function updatePost(url) {
 
   // Hämtar data från fält
   const name = document.getElementById("newName").value;
-  const level = document.getElementById("level").value;
   console.log(name);
-  //Skapa ett objekt med data inkluderat.
+
+  //Skapa ett objekt innehållande data 
   let memeObjekt = {
-      data : {}
+    data: {},
   };
 
-  //Fyller upp Data med parameter-värden
+  //Fyller upp data med värden från inmatning
   if (name) memeObjekt.data["meme"] = name;
-  if (level) memeObjekt.data["chucklevel"] = level;
 
-  //Anropar API med pokemonObjekt
-  await fetch(url,
-  {
-      method: 'PUT',
-      headers: {
-          "Content-Type": "application/json",
-          "Authorization" : "Bearer " + token //Inkluderar Token från inloggning tidigare.
-      },
-      body: JSON.stringify(memeObjekt)
+  //Anropar API med memeObjekt. Inkluderar Token från inloggning 
+  await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token, 
+    },
+    body: JSON.stringify(memeObjekt),
   });
 
-  //Anropa "GetDataFromStrapi" för att skriva ut ny tabell
+  //skriver ut databasen med tillhörande utskrift igen
   await getDataFromStrapi();
 }
 
-
-
-
-//DELETE FUNCTION
+//deletefunktion
 async function deleteMeme(url) {
-
-  //Hämta Token från GetToken()
+  //Hämta Token från getToken()
   //Om ingen Token returneras, avbryt funktionen
   let token = await getToken();
   if (!token) return;
 
   //Anropar API med inloggningsdata.
-  //Inkluderar Method och Headers
-  await fetch(url,
-      {
-          method: 'DELETE',
-          headers: {
-              "Content-Type": "application/json",
-              "Authorization" : "Bearer " + token //Inkluderar Token från inloggning tidigare.
-          }
-      });
-      getDataFromStrapi();
-    }
+  await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token, 
+    },
+  });
+  //skriver ut databasen med tillhörande utskrift
+  getDataFromStrapi();
+}
 
+//funktion med inputfält för validering för delete-funktionen
+async function deleteConfirm(url, x) {
+  let output = "";
 
-
-    async function deleteConfirm(url, x) {
-      let test = true;
-      let output = "";
-    
-      if (test) {
-        output += `<div>
+  //skriver ut fält för inlogg, då validering behövs för delete-funktion
+    output += `<div>
         <h1>DELETE MEME </h1>
         <div>
-                <h1>Ange användarnamn och lösenord för att kunna ta bort memes: </h1>
-                <label for="user">Användarnamn:</label>
+                <h1>Enter username and password to remove meme: </h1>
+                <label for="user">Username:</label>
                 <input type="text" name="user" id="user">
         
-                <label for="pass">Lösenord:</label>
+                <label for="pass">Password:</label>
                 <input type="text" name="pass" id="pass">
             </div><br><br>
        
         <button onclick="deleteMeme('${url}');">DELETE</button>
-      </div>`
-      }
-    
-      document.getElementById('editMeme'+x).innerHTML = output;
-      
-    }
-
-
-
-    //Genererat tabellrad med det inkludera objektet. Skapar TH rad om header=true
-function generateRow(obj, objId, header) {
-
-  let editOutput = "<tr>";
-  let forbiddenParameters = ["createdAt", "updatedAt", "publishedAt"];
-
-  //For in loop för att gå igenom alla parametrar i obj
-  for (x in obj) {
-      /*
-      x = parameterns namn
-      obj[x] = parameterns värde
-      */
-
-      //Kontrollera att x är en tillåten parameter.
-      // Keyword Continue går vidare till nästa parameter i loopen
-      //Fungerar också i en ForEach loop.
-      if (forbiddenParameters.includes(x)) continue;
-
-      if (header) editOutput += `<th>${x}</th>`;
-      else        editOutput += `<td>${obj[x]}</td>`;
-  }
-
-  //Skapa update och Delete knapp för TD rad
-  if (!header) {
-      //URL för den specifika posten
-      let postURL = `http://localhost:1337/api/memes/${objId}`;
-
-      editOutput += `<td><button onclick="updatePost('${postURL}');">Update Post</button></td>`;
-      editOutput += `<td><button onclick="deletePost('${postURL}');">Delete Post</button></td>`;
-  }
-
-  //Stänga <tr> taggen
-  editOutput += "</tr>";
-
-  return editOutput;
+      </div>`;
+  
+  document.getElementById("editMeme" + x).innerHTML = output;
 }
 
 
-  
